9e93195cd787f629a2a543a68ded570130a9d7e2
Mateus Araújo Monteiro <129124225+MMatteuss@users.noreply.github.com>
2024-08-28 14:31:43 -0300

    tirando as linhas de comentários padrões da linguagem Java

--- Senac/atividade1/src/atividade1/Questao2.java
+++ Senac/atividade1/src/atividade1/Questao2.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package atividade1;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Mateus
- */
 public class Questao2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in, "UTF-8");
         
