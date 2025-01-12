3f65ebcb89bd5878a01a23f552ffc68d69ad2152
Mateus Araújo Monteiro <129124225+MMatteuss@users.noreply.github.com>
2025-01-12 17:29:38 -0300

    projeto filmes

---
+++ Formulário de contato com validação/app/js/main.js
@@ -0,0 +1,8 @@
+var primeiroNome = document.getElementById("primeiroNome");
+var sobrenome = document.getElementById("");
+var email = document.getElementById("");
+var telefone = document.getElementById("");
+var endereco = document.getElementById("");
+var mensagem = document.getElementById("");
+
+primeiroNome.textContent("Ola")
